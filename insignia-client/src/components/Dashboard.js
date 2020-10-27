43e264666da7d3e03173a0c09559e5d9708e279f
Calvin <calvin_espinoza@hotmail.com>
2020-10-27 00:17:53 -0600

    dashboard ui 2.0

--- insignia-client/src/components/Dashboard.js
+++ insignia-client/src/components/Dashboard.js
@@ -6,7 +6,7 @@ import levelIcon from '../assets/level-icon.png';
 import logoInsignia from '../assets/insignia-light.svg';
 
 import { Bar } from 'ant-design-pro/lib/Charts';
-import { Layout, Menu, Statistic, Card, Row, Col, Empty } from 'antd';
+import { Layout, Menu, Statistic, Card, Row, Col, Empty, Progress } from 'antd';
 import {
   BarChartOutlined,
   LogoutOutlined,
@@ -24,10 +24,11 @@ export class Dashboard extends React.Component {
       loading: true,
       user: {
         Statistics: [],
-        LevelProgress: {}
+        LevelProgress: { XP: 0 }
       },
       displayName: "",
-      userPoints: []
+      userPoints: [],
+      nextLevelXP: 0
     };
   }
 
@@ -41,16 +42,17 @@ export class Dashboard extends React.Component {
         response.json()
       )
       .then((userData) => {
-        this.mapUserResult(userData)
+        this.mapUserResult(userData);
         this.setState({
           user: userData,
-          //loading: false
         })
       })
   }
 
   mapUserResult(userData) {
     var points = [];
+    var nextLvl = 0;
+    var nextLevelXP = 0;
 
     if (userData.PointsHistory) {
       for (let i = 0; i < userData.PointsHistory.length; i++) {
@@ -63,22 +65,31 @@ export class Dashboard extends React.Component {
       }
     }
 
+    if (userData.LevelProgress) {
+      nextLvl = userData.LevelProgress.CurrentLevel + 1;
+      nextLevelXP = 5000 * Math.pow(nextLvl, 2) - 5000 * nextLvl
+    }
+
+
     this.setState({
       displayName: userData.DisplayName,
-      userPoints: points
+      userPoints: points,
+      nextLevelXP: nextLevelXP
     })
   }
 
   render() {
-    const statistics = this.state.user.Statistics.map((stat) => {
+    const statistics = this.state.user.Statistics.map((stat, i) => {
+      var pos = i % 4;
       return (
         <Col xs={24} md={12} xl={6} >
-          <Card className="dashboard-card" style={{ width: '100%' }}>
+          <Card className="dashboard-card statistic-card"
+            style={{ width: '100%', backgroundColor: colorConfigs[pos].background }}>
+            <h4 style={{ color: colorConfigs[pos].foreground }}>{stat.StatisticName}</h4>
             <Statistic
-              title={stat.StatisticName}
               value={stat.Value}
               //precision={2}
-              valueStyle={{ color: /*'#007bff'*/'black', fontSize: '2.5vmax' }}
+              valueStyle={{ color: colorConfigs[pos].foreground, fontSize: '3em' }}
               //prefix={<ArrowUpOutlined />}
               suffix={stat.Suffix}
               style={{ fontSize: 14 }}
@@ -90,79 +101,87 @@ export class Dashboard extends React.Component {
     return (
       <Layout>
         <Sidebar />
-        <Content style={{ padding: '2em 5em', backgroundColor: '#f9f9f9', }} >
-          <h2 className="main-subtitle">Hola, {this.state.user.DisplayName}!</h2>
-          <Row justify="space-between" align="middle">
-            <Col>
-              <h1 className="main-heading">Overview</h1>
+        <Content className="dashboard-content" style={{ padding: '2em 8em', backgroundColor: '#f9f9f9', }} >
+          <Row justify="space-between" align="bottom" gutter={[24, 24]}>
+            <Col xs={24} lg={16} xl={18} >
+              <h2 className="main-subtitle">Hola, {this.state.user.DisplayName}!</h2>
+              <h1 className="main-heading">Resumen</h1>
             </Col>
-            <Col sm={24} md={12} lg={4} style={{ fontSize: 28 }}>
-              <Row align='middle' >
+            <Col xs={24} lg={8} xl={6} style={{ fontSize: 24 }}>
+              <Row align='middle' justify='end'>
                 <h2 style={{ margin: 0 }}>Lvl {this.state.user.LevelProgress.CurrentLevel}</h2>
-                <img src={levelIcon} alt='level-icon' style={{ width: 60, height: 60, margin: 10 }} />
+                <img src={levelIcon} alt='level-icon' style={{ width: 55, height: 55, margin: 10 }} />
+              </Row>
+              <Row justify='end'>
+                <Progress
+                  percent={this.state.user.LevelProgress.NextLevelProgress * 100}
+                  strokeColor="#2FDF84"
+                  trailColor="#EAEAEA"
+                  showInfo={false} />
+              </Row>
+              <Row justify='end'>
+                <span style={{ fontSize: 16 }}>
+                  {this.state.user.LevelProgress.XP.toLocaleString()} XP / {this.state.nextLevelXP.toLocaleString()} XP
+                </span>
               </Row>
             </Col>
           </Row>
-          <Row gutter={[16, 16]}>
-            <Col span={24}>
-              <Row gutter={[16, 16]} justify='start'>
-                {statistics}
-              </Row>
-              <Row gutter={[16, 16]}>
-                <Col xs={24} xl={18}>
-                  <Card className='dashboard-card' style={{ padding: '0 2em' }}>
-                    <div style={{ marginBottom: 30 }}>
-                      <h2 className='h2-dashboard' style={{ margin: 0 }}>Historial de Puntos</h2>
-                      <span>Basado en tus últimos 10 intentos</span>
-                    </div>
-                    {
-                      this.state.userPoints.length > 0 ?
-                        <Bar height={300} data={this.state.userPoints} color='#2FDF84' />
-                        :
-                        <Empty />
-                    }
-                  </Card>
-                </Col>
-                <Col xs={24} xl={6}>
-                  <Card className="dashboard-card" >
-                    <h2 className='h2-dashboard'>Datos de Usuario</h2>
-                    <Row>
-                      <Col xs={8} xl={24}>
-                        <Row gutter={4}>
-                          <span>Nombre</span>
-                        </Row>
-                        <Row gutter={4}>
-                          <h2>{this.state.user.DisplayName}</h2>
-                        </Row>
-                      </Col>
-                      <Col xs={8} xl={24}>
-                        <Row gutter={4}>
-                          <span>Pais</span>
-                        </Row>
-                        <Row gutter={4}>
-                          <h2>{this.state.user.Country}</h2>
-                        </Row>
-                      </Col>
-                      <Col xs={8} xl={24}>
-                        <Row gutter={4}>
-                          <span>Edad</span>
-                        </Row>
-                        <Row gutter={4}>
-                          <h2>{this.state.user.Age}</h2>
-                        </Row>
-                      </Col>
-                      <Col xs={24} sm={12} md={8} xl={24}>
-                        <Row gutter={4}>
-                          <span>Escuela</span>
-                        </Row>
-                        <Row gutter={4}>
-                          <h2>{this.state.user.School}</h2>
-                        </Row>
-                      </Col>
+          <Row gutter={[24, 24]}>
+            {statistics}
+          </Row>
+          <Row gutter={[24, 24]}>
+            <Col xs={24} xl={18}>
+              <Card className='dashboard-card' style={{ padding: '0 2em' }}>
+                <div style={{ marginBottom: 30 }}>
+                  <h2 className='h2-dashboard' style={{ margin: 0 }}>Historial de Puntos</h2>
+                  <span style={{fontSize: 16}}>Basado en tus últimos 10 intentos</span>
+                </div>
+                {
+                  this.state.userPoints.length > 0 ?
+                    <Bar height={300} data={this.state.userPoints} color='#2FDF84' />
+                    :
+                    <Empty />
+                }
+              </Card>
+            </Col>
+            <Col xs={24} xl={6}>
+              <Card className="dashboard-card" >
+                <h2 className='h2-dashboard'>Datos de Usuario</h2>
+                <Row>
+                  <Col xs={8} xl={24}>
+                    <Row gutter={4}>
+                      <span>Nombre</span>
                     </Row>
-                  </Card>
-                </Col>
-              </Row>
+                    <Row gutter={4}>
+                      <h2>{this.state.user.DisplayName}</h2>
+                    </Row>
+                  </Col>
+                  <Col xs={8} xl={24}>
+                    <Row gutter={4}>
+                      <span>Pais</span>
+                    </Row>
+                    <Row gutter={4}>
+                      <h2>{this.state.user.Country}</h2>
+                    </Row>
+                  </Col>
+                  <Col xs={8} xl={24}>
+                    <Row gutter={4}>
+                      <span>Edad</span>
+                    </Row>
+                    <Row gutter={4}>
+                      <h2>{this.state.user.Age}</h2>
+                    </Row>
+                  </Col>
+                  <Col xs={24} sm={12} md={8} xl={24}>
+                    <Row gutter={4}>
+                      <span>Escuela</span>
+                    </Row>
+                    <Row gutter={4}>
+                      <h2>{this.state.user.School}</h2>
+                    </Row>
+                  </Col>
+                </Row>
+              </Card>
             </Col>
           </Row>
         </ Content>
@@ -181,7 +200,7 @@ const Sidebar = () => {
       <img src={logoInsignia} alt="Insignia logo" style={{ height: 45, margin: 20 }}></img>
       <Menu theme="light" mode="inline" defaultSelectedKeys={['1']}>
         <Menu.Item key="1" icon={<BarChartOutlined />}>
-          Overview
+          Resumen
         </Menu.Item>
         <Menu.Item key="2" icon={<BankOutlined />} disabled>
           Escuela
@@ -204,3 +223,10 @@ const Sidebar = () => {
     </Sider>
   )
 }
+
+const colorConfigs = [
+  { "name": "blue", "background": "#E8F4FC", "foreground": "#2461CB" },
+  { "name": "yellow", "background": "#FCF8E8", "foreground": "#CB7424" },
+  { "name": "purple", "background": "#F4E8FC", "foreground": "#6D24CB" },
+  { "name": "red", "background": "#FCE8E8", "foreground": "#CB2424" },
+]
